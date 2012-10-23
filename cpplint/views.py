e10d9609de77202c065a084eee5be4dcc695aa38
Alex Roper <aroper@umich.edu>
2012-10-23 13:18:05 -0400

    We now use a plugin-based framework to support easy extensions.

--- cpplint/views.py
+++ cpplint/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django import forms
 from django.template.defaultfilters import escape
 
+import collections
+
 def space_escape(s):
   return escape(s).replace("\t", "&nbsp;&nbsp;").replace(" ", "&nbsp;")
 
@@ -28,25 +30,31 @@ def upload(request):
     form = UploadFileForm(request.POST, request.FILES)
     if form.is_valid():
       their_code = request.FILES['file'].read()
-      stdin, stdout, stderr = os.popen3("python %s/cpplint/cpplint.py --filter=-legal,-readability/streams,-whitespace/newline,-readability/constructors,-runtime/arrays,-build/namespaces,-runtime/string,-readability/casting,-runtime/references,-readability/streams,-whitespace/labels,-readability/todo,-runtime/threadsafe_fn,-readability/header_guard,-whitespace/braces_google,-build/include_directory -" % os.getcwd())
-      stdin.write(their_code)
-      stdin.close()
+      lint = collections.defaultdict(list)
+      for plugin in open("cpplint/plugins.conf"):
+        try:
+          plugin, _ = plugin.split("#", 1)
+        except ValueError:
+          pass
+        plugin = plugin.strip()
 
-      lint = {}
-      for line in stdout:
-        if line.startswith("Done processing") or line.startswith("Total errors found"):
-          continue
-        line_no, comment = line[2:].split(":", 1)
-        lint[max(0, int(line_no) - 1)] = comment
+        if plugin:
+          stdin, stdout, stderr = os.popen3("bash -c '%s'" % plugin)
+          stdin.write(their_code)
+          stdin.close()
+          for line in stdout:
+            fn, line_no, comment = line.split(":", 2)
+            lint[max(0, int(line_no) - 1)].append(comment)
 
       response = ""
       for (line_no, line) in enumerate(their_code.split("\n")):
         if line_no in lint:
           response += "<font color=red>%s</font><br/>" % space_escape(line) + "\n"
-          response += "<font color=green>// ^^^ SUGGESTION: %s</font><br/>" % space_escape(lint[line_no].replace("\n", "")) + "\n"
+          for comment in lint[line_no]:
+            response += "<font color=green>// ^^^ SUGGESTION: %s</font><br/>" % space_escape(comment.replace("\n", "")) + "\n"
         else:
           response += space_escape(line) + "<br/>" + "\n"
 
-      return HttpResponse("<h1>Comments will appear in green and the lines they apply to in red.</h1><h2> There are a total of %i suggestions for this file.</h2>" % len(lint) + response)
+      return HttpResponse("<h1>Comments will appear in green and the lines they apply to in red.</h1><h2> There are a total of %i suggestions for this file.</h2>" % sum(map(len, lint.values())) + response)
     else:
       return HttpResponseRedirect('/cpplint/invalid')
