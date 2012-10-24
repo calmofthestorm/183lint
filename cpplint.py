65027071d3f3dbcce065fc897d07d0d54977bc88
Alex Roper <aroper@umich.edu>
2012-10-23 21:27:56 -0400

    Only want to require one space between code and comments.

--- cpplint.py
+++ cpplint.py
@@ -1722,13 +1722,9 @@ def CheckSpacing(filename, clean_lines, linenum, error):
     if (line.count('"', 0, commentpos) -
         line.count('\\"', 0, commentpos)) % 2 == 0:   # not in quotes
       # Allow one space for new scopes, two spaces otherwise:
-      if (not Match(r'^\s*{ //', line) and
-          ((commentpos >= 1 and
-            line[commentpos-1] not in string.whitespace) or
-           (commentpos >= 2 and
-            line[commentpos-2] not in string.whitespace))):
+      if (commentpos >= 1 and line[commentpos-1] not in string.whitespace):
         error(filename, linenum, 'whitespace/comments_two', 2,
-              'At least two spaces is best between code and comments')
+              'At least one space is best between code and comments')
       # There should always be a space between the // and the comment
       commentend = commentpos + 2
       if commentend < len(line) and not line[commentend] == ' ':
